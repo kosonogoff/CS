992388be998d0c2844627c30f2b466bddecd0fef
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-16 13:29:17 +0300

    add in Example 003 Random Numbers

--- lesson/003_SumNumbers/Program.cs
+++ lesson/003_SumNumbers/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 30;
-int numberB = 51;
+﻿// сумма двух случайных чисел
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.Write("Первое случайное число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.Write("Второе случайное число: ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("Сумма чисел: ");
 Console.WriteLine(result);

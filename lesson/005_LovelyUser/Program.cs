15627f9c6b0cc31cade0d6ca95ef3c8092caa27a
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-16 13:40:20 +0300

    add 005 Lovely User

---
+++ lesson/005_LovelyUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "паша")
+{
+    Console.WriteLine("Приветствую тебя, Мой Господин!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

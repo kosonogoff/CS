2934517df916c967ac3663ae42a41a0880810184
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-20 12:25:07 +0300

    add homework task 004 Max Number

---
+++ homework/004_MaxNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три ЦЕЛЫХ числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое целое число: "); // запрос первого числа от пользователя
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: "); // запрос второго числа от пользователя
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число: "); // запрос третьего числа от пользователя
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+    {
+        max = numberB;
+    }
+
+if (numberC > max)
+    {
+        max = numberC;
+    }
+
+Console.Write($"Максимальное введённое число: {max}");

c31516b0f1d1198da4fc108d3834d8aa140268c9
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-20 12:04:52 +0300

    add homework task 002 Min Max

---
+++ homework/002_MinMax/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два ЦЕЛЫХ числа и выдаёт, какое число большее, а какое меньшее
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое целое число: "); // запрос первого числа от пользователя
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: "); // запрос второго числа от пользователя
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int min = numberA;
+int max = numberB;
+
+if (max > min)
+{
+Console.WriteLine($"Минимальное число: {min}"); // вывод результата
+Console.WriteLine($"Максимальное число: {max}");
+}
+else
+{
+    min = numberB;
+    max = numberA;
+
+Console.WriteLine($"Минимальное число: {min}"); // вывод результата
+Console.WriteLine($"Максимальное число: {max}");
+}

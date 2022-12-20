bcfd2be3d4deb2964e6bb144db6ff59c8b0b0b38
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-20 13:11:26 +0300

    add homework task 008 All Even

---
+++ homework/008_AllEven/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает ЦЕЛОЕ число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int start = 2;
+int print = start;
+
+Console.Write($"{print} ");
+
+// сейчас цикл запустим
+while (start < number - 1) 
+{
+    print = print + 2;
+    Console.Write($"{print} ");
+    start = start + 2;
+}

b9542687111b979e98242b9bbb65c835da47d063
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-25 13:48:00 +0300

    add 010 Method Array

---
+++ lesson/010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, 35, 76, 12, 867, 456, 765, 1, 2150};
+int n = array.Length;
+int find = 1;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}

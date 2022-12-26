d339752615e77cb9a290406c039320fe12a918d3
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-26 16:53:09 +0300

    add 003 Shorter Number

---
+++ seminar/003_ShorterNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное число: {number}");
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int numberNew = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine($"Новое число: {numberNew}");

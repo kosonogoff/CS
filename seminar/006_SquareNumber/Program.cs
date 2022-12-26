615b65c983cb93ea8266e8aa28224fc75aaa08a1
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-26 19:58:07 +0300

    add 006 Square Number

---
+++ seminar/006_SquareNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход два ЦЕЛЫХ числа 
+// и проверяет, является ли одно число квадратом другого.
+
+Console.Write("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < numberB)
+{
+    int result = numberB % numberA;
+    if (result == 0)
+    {
+        Console.WriteLine($"Да, число {numberB} является квадратом числа {numberA}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Нет, число {numberB} не является квадратом числа {numberA}");
+    }
+}
+
+else
+{
+    int temp = numberA;
+    numberA = numberB;
+    numberB = temp;
+    int result = numberB % numberA;
+    if (result == 0)
+    {
+        Console.WriteLine($"Да, число {numberB} является квадратом числа {numberA}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Нет, число {numberB} не является квадратом числа {numberA}");
+    }
+}

627d22d7b0aa758437a0f830a9dcef0b04a60a12
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-26 19:40:31 +0300

    add 005 Multiple

---
+++ seminar/005_Multiple/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход ЦЕЛОЕ число
+// и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int div1 = number % 7;
+int div2 = number % 23;
+
+if (div1 == 0)
+{
+    if (div2 == 0)
+    {
+        Console.WriteLine("Да, число кратно 7 и 23");
+    }
+
+    else
+    {
+    Console.WriteLine("Нет, число не кратно 7 и 23");
+    }
+}
+
+else
+{
+    Console.WriteLine("Нет, число не кратно 7 и 23");
+}

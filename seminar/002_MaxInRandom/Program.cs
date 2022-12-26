1475d9adc29717ed9bb9aab83a46e0bb41a69c4c
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-26 16:28:40 +0300

    add 002 Max In Random

---
+++ seminar/002_MaxInRandom/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное ЦЕЛОЕ число
+// из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100); // Random number 11-99
+
+int firstDigit = number / 10;
+int secondDigit = number % 10; // остаток от целочисленного деления
+
+Console.WriteLine($"Случайное число: {number}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+
+if (secondDigit > firstDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+
+if (firstDigit == secondDigit)
+{
+    Console.WriteLine($"Цифры в числе равны");
+}

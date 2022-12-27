641cb2fdeba890c649c939867603663fe7702470
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-27 03:22:31 +0300

    add homework task 010 Second Digit

---
+++ homework/010_SecondDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿// SНапишите программу, которая принимает на вход трёхзначное ЦЕЛОЕ число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int workNumber = number / 10;
+int secondDigit = workNumber % 10;
+
+Console.WriteLine ($"Вторая цифра введённого числа {secondDigit}");

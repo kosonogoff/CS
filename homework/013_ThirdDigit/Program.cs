24c114d254a2cbb3cc904d8a604dc5105c581b37
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-27 17:30:21 +0300

    add homework task 013 Third Digit

---
+++ homework/013_ThirdDigit/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит третью цифру заданного ЦЕЛОГО числа 
+// или сообщает, что третьей цифры нет.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = -number;
+}
+
+if (number < 100)
+{
+    Console.WriteLine($"В данном числе нет третьей цифры");
+}
+
+else if (number >= 100 && number <= 999)
+{
+    int ThirdDigit = number % 10;
+    Console.WriteLine($"Третья цифра заданного числа {ThirdDigit}");
+}
+
+else if (number >= 1000)
+{
+    
+    int result = number / 10;
+    
+    while (result >= 9999)
+    {
+        result = result / 10;
+    }
+    
+    int ThirdDigit = result % 10;
+    Console.WriteLine($"Третья цифра заданного числа {ThirdDigit}");
+}

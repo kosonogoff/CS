ccbe3cb32d5af8417aed001b07fb6336299ff236
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-30 16:02:22 +0300

    add 022 Square Of Numbers

---
+++ seminar/022_SquareOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход ЦЕЛОЕ число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+/* int start = 1;
+int i = 1;
+while (i <= number)
+{
+    Console.Write($"{Math.Pow(start, 2)} ");
+    start++;
+    i++;
+} */
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i} ");
+}

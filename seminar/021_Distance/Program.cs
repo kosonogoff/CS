70d6aee638ca9e232f134feef554d9281c6a2b5a
Pavel <60812654+kosonogoff@users.noreply.github.com>
2022-12-30 14:59:39 +0300

    add 021 Distance

---
+++ seminar/021_Distance/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве
+
+Console.Write("Введите значение X для первой точки: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение Y для первой точки: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение X для второй точки: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение Y для второй точки: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine($"Расстояние между точками равняется {result}");

86fad89fff4ef7eb92e1a8a8b69416890de4d174
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 21:18:05 +0300

    add 026 Number Length

---
+++ seminar/026_NumberLength/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход ЦЕЛОЕ число
+// и выдаёт количество цифр в числе.
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLength(int number)
+{
+    int length = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        length = length + 1;
+    }
+    return length;
+}
+
+int number = ReadNumber("Введите число: ");
+int length = NumberLength(number);
+Console.Write($"В числе {number} количеcтво цифр равно {length}");

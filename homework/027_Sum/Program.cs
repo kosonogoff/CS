bc6626174424b42a3b6054ead20235537b7bb091
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 22:49:13 +0300

    add homework task 027 Sum

---
+++ homework/027_Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход ЦЕЛОЕ число 
+// и выдаёт сумму цифр в числе
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumInTotal(int a)
+{
+    int result = 0;
+    while (a != 0)
+    {
+        int remainder = a % 10;
+        result = result + remainder;
+        a = a / 10;        
+    }
+    return result;
+}
+
+int number = ReadNumber("Введите число: ");
+
+if(number > 0)
+{
+    int sum = SumInTotal(number);
+    Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
+}
+
+else if(number == 0)
+{
+    Console.WriteLine($"Это же ноль. Здесь всё очевидно!");
+}
+
+else if(number < 0)
+{
+    int sum = SumInTotal(-number);
+    Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
+}

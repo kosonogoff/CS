5085f1eed034bbd26c73ef1c3994c40bccedf804
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 20:42:07 +0300

    add 024 Sum Numbers

---
+++ seminar/024_SumNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает ЦЕЛОЕ число "А"
+// и находит сумму всех чисел от 1 до "А"
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum1ToA(int a)
+{
+    int sum = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        sum = sum + i; // sum +- i;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+
+if(number > 0)
+{
+    int sum = GetSum1ToA(number);
+    Console.Write($"Сумма чисел от 1 до {number} равна {sum}");
+}
+
+else
+{
+    Console.Write("Введено неверное значение. Число должно быть больше нуля");
+}

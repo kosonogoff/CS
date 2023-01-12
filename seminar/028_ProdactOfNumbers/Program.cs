be5d72a6f4ae2081d8b843ebd255c61e5f447993
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 21:35:56 +0300

    add 028 Prodact Of Numbers

---
+++ seminar/028_ProdactOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetProdact(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int number = ReadNumber("Введите число: ");
+int prodact = GetProdact(number);
+Console.WriteLine($"Произведение чисел равно {prodact}");

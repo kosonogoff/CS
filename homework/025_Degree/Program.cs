8cad1c6be2f43ff3bd3d5c76ca00b8a2f773a5bf
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 22:31:28 +0300

    add homework task 025 Degree

---
+++ homework/025_Degree/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int Degree(int a, int b)
+{
+    int result = a;
+    for (int i = 2; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int a = ReadNumber("Введите первое число: ");
+int b = ReadNumber("Введите второе число: ");
+int degree = Degree(a, b);
+Console.WriteLine($"{a} в степени {b} будет равно {degree}");

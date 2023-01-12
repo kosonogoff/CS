4822a09b1023b6cf3e5480fb85c9c0a6f138dd19
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-12 22:08:08 +0300

    add 030 Array

---
+++ seminar/030_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке
+
+int[] FillRandomArray(int size)
+{
+    int[] arr = new int[size]; // выделили память для массива
+
+    for (int i = 0; i < arr.Length; i++) // C# знает длину массива
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+int[] array = FillRandomArray(8);
+Console.WriteLine($"[{string.Join(",", array)}]");

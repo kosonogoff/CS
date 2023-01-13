61304019b64e7af242938b61d9fe850575778e50
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-13 21:24:24 +0300

    add homework task 029 Create Array

---
+++ homework/029_CreateArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+int[] CreateNewArray(int size)
+{
+    int[] arr = new int [size];
+    return arr;
+}
+
+int size = 8; // здесь прописываем размер массива
+int[] array = CreateNewArray(size);
+
+int FillingArray(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int position = 0;
+
+while (position < size)
+{
+    int number = FillingArray("Введите значение: ");
+    array[position] = number;
+    position++;
+}
+
+Console.WriteLine($"[{string.Join(",", array)}]");

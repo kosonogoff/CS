6c904c11420ad11ed3d83cab354cfe564e29a86c
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-16 21:00:57 +0300

    add 032 Array Replacement

---
+++ seminar/032_ArrayReplacement/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу замены элементов массива: положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+
+int[] GetRandomArray (int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange +1);
+    }
+    return array;
+}
+
+const int size = 10;
+const int leftRange = -100;
+const int rightRange = 100;
+
+int[] massive = GetRandomArray (size, leftRange, rightRange);
+Console.Write("Данный массив: [");
+Console.Write(string.Join(", ", massive));
+Console.WriteLine("]");
+
+int[] ArrayReplacement (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+massive = ArrayReplacement (massive);
+Console.Write("Новый массив: [");
+Console.Write(string.Join(", ", massive));
+Console.WriteLine("]");

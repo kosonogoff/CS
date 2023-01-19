6a8a4597711ac63116c6da4b338442c21fd77d28
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-19 22:04:46 +0300

    add 040 Triangle

---
+++ seminar/040_Triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и проверяет, может ли существовать треугольник с сторонами такой длины.
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool Treangle (int x, int y, int z)
+{
+    if ( (x+y>z) && (x+z>y) && (z+y>x))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine($"Такой треугольyик существует: {Treangle(a, b, c)}");

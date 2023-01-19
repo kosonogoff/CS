da016850d5b37373d99f476ad797afb7347d4ab5
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-19 21:29:38 +0300

    add 042 Converting To Binary

---
+++ seminar/042_ConvertingToBinary/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+int ReadNumber(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+string ConvertingToBinary(int decimalNumber)
+{
+    var binaryNumber = string.Empty;
+    while (decimalNumber > 0)
+    {
+        binaryNumber = (decimalNumber % 2) + binaryNumber;
+        decimalNumber /= 2; // decimalNumber = decimalNumber / 2;
+    }
+    return binaryNumber;
+}
+
+int number = ReadNumber("Введите число: ");
+var binary = ConvertingToBinary(number);
+Console.WriteLine($"{number} в двоичной системе выглядит как {binary s}");

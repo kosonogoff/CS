24b4327bc7364b50578c3bb3be0d019a9c1eb06b
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-21 16:49:15 +0300

    add 014 Two-Dimensional Array

---
+++ lesson/014_TwoDimensionalArray/Program.cs
@@ -0,0 +1,33 @@
+﻿// ДВУМЕРНЫЕ (МНОГОМЕРНЫЕ) МАССИВЫ
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    // matr.GetLength(0) возвращает количество строк в массиве
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        // matr.GetLength(1) возвращает количество столбцов в массиве
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(10,100);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine(); // добавим пустую строку для красоты
+PrintArray(matrix);

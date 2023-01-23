cc1648e452f6667e1ecc1af309f0c9be4ed5e488
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-23 20:52:02 +0300

    add 048 Fill Array[,]

---
+++ seminar/048_FillArray[,]/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] GetMatrix (int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int [rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void FillArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+}
+
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int rows = 3;
+const int columns = 4;
+int min = 0;
+int max = 9;
+
+int[,] array = GetMatrix (rows, columns, min, max);
+FillArray(array);
+PrintMatrix(array);

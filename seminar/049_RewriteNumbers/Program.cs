4ac7b9ceff5051fbcb0da881ee60d49781d223f0
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-23 21:29:30 +0300

    formatted code

--- seminar/049_RewriteNumbers/Program.cs
+++ seminar/049_RewriteNumbers/Program.cs
@@ -12,9 +12,9 @@
 // 8 4 2 4
 
 
-int[,] GetMatrix (int rows, int columns, int min, int max)
+int[,] GetMatrix(int rows, int columns, int min, int max)
 {
-    int[,] matrix = new int [rows, columns];
+    int[,] matrix = new int[rows, columns];
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -26,28 +26,28 @@ int[,] GetMatrix (int rows, int columns, int min, int max)
     return matrix;
 }
 
-void PrintMatrix (int [,] matrix)
+void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            int number = matrix[i,j];
-            if (number < 10) Console.Write("0" + matrix[i,j] + " ");
+            int number = matrix[i, j];
+            if (number < 10) Console.Write("0" + matrix[i, j] + " ");
             // для чисел 1-9 добавляем 0 впереди, чтобы было визуально приятнее
-            else Console.Write(matrix[i,j] + " ");
+            else Console.Write(matrix[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-void RewriteNumbersInMatrix (int[,] matrix)
+void RewriteNumbersInMatrix(int[,] matrix)
 {
     for (int i = 1; i < matrix.GetLength(0); i += 2)
     {
         for (int j = 1; j < matrix.GetLength(1); j += 2)
         {
-            matrix[i,j] *= matrix[i,j];
+            matrix[i, j] *= matrix[i, j];
         }
     }
 }
@@ -57,7 +57,7 @@ int columns = 4;
 int min = 1;
 int max = 9;
 
-int[,] array = GetMatrix (rows, columns, min, max);
+int[,] array = GetMatrix(rows, columns, min, max);
 PrintMatrix(array);
 RewriteNumbersInMatrix(array);
 Console.WriteLine();

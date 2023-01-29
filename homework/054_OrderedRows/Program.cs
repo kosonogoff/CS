40c2a806440869e33bd540f2c19c752a8c9752c6
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-01-29 20:21:49 +0300

    add homework task 054

---
+++ homework/054_OrderedRows/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            if (matrix[i, k] > -1 && matrix[i, k] < 10) Console.Write(" " + matrix[i, k] + " ");
+            else Console.Write(matrix[i, k] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderedRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) /// ???
+        {
+            int max = matrix[i, j];
+            int indexMax = j + 0;
+            int index = j + 1;
+
+            while (index < matrix.GetLength(1))
+            {
+                if (matrix[i, index] > max)
+                {
+                    max = matrix[i, index];
+                    indexMax = index;
+                }
+                index++;
+            }
+
+            int temp = matrix[i, j];
+            matrix[i, j] = max;
+            matrix[i, indexMax] = temp;
+        }
+    }
+}
+
+int rows = new Random().Next(5, 10);
+int columns = new Random().Next(10, 20);
+int min = -9;
+int max = 99;
+
+int[,] matrixRandom = GetMatrix(rows, columns, min, max);
+PrintMatrix(matrixRandom);
+Console.WriteLine();
+
+OrderedRows(matrixRandom);
+PrintMatrix(matrixRandom);

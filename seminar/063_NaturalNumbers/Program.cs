73f8bdbf4ab28fbf30288afd69d1cf1aba1583e4
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-02-01 21:01:32 +0300

    add 063 Natural Numbers

---
+++ seminar/063_NaturalNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbersUpRec(int n)
+{
+    if (n == 0) return;
+    PrintNumbersUpRec(n - 1);
+    Console.Write(n + " ");
+}
+
+int number = ReadNumber("Введите число: ");
+PrintNumbersUpRec(number);

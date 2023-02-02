41b9435d94cb644470812325819fc9025ecd3f9a
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-02-02 20:56:51 +0300

    add homework task 064

---
+++ homework/064_NaturalNumbersRec/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// от большего к меньшему
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumberDownRec(int number, int finish = 1)
+{
+    if (finish > number) return;
+    PrintNumberDownRec(number, finish + 1);
+    Console.Write(finish + " ");
+}
+
+int number = ReadNumber("Введите число: ");
+PrintNumberDownRec(number);

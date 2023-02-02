24b46b2f05e8bd84abe1d23d354c7fe891f680c6
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-02-02 07:18:25 +0300

    add 067 Sum Numbers Recursion

---
+++ seminar/067_SumNumbresRec/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход ЦЕЛОЕ число 
+// и возвращать сумму его цифр
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumRec(int number)
+{
+    if (number < 10) return number;
+    return (number % 10) + GetSumRec(number / 10);
+}
+
+int number = ReadNumber("Введите число: ");
+Console.Write($"Сумма цифр в числе равна: {GetSumRec(number)}");

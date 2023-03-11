5e62247fa170e2392c81b5bd2785e047728aa079
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-03-11 10:16:04 +0300

    start 02 Linear Equation

---
+++ bonus/02_LinearEquation/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать решение уравнения линейного типа
+// a*m + b*n = c
+// Задача на передачу денежных средств
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintIndex(int[]array, int index)
+{
+    return array[index];
+}
+
+/*
+int[] Eukleides(int[] array)
+{
+    int[] matrix = new int[10];
+
+
+
+    return martix;
+}
+*/
+
+int a = ReadNumber("Введите размер первой купюры: ");
+int b = ReadNumber("Введите размер второй купюры: ");
+int sum = ReadNumber("Введите размер суммы, которую нужно передать: ");
+
+int[] input = new int[3] { a, b, sum };
+
+Console.Write($"Нужно решить уравнение типа {PrintIndex(input, 0)}m + {PrintIndex(input, 1)}n = {PrintIndex(input, 2)}");
+
+// a * x + b * y = 1
+
+// m = x + b * t
+// n = y - a * t
